5dc6a14405a13d32c2e2a04f5c6c9697c2a52430
unknown <chandrasen.singh1@gmail.com>
2021-07-19 11:36:11 +0000

    new change

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace corewebappforazure
 {
     public class Program
     {
+        //test comment
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
